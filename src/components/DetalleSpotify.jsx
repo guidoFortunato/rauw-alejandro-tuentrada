70018bba41f3e03dd89fca86282b949df7655c35
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2023-06-27 17:52:23 -0300

    detalle spotify iframe

--- src/components/DetalleSpotify.jsx
+++ src/components/DetalleSpotify.jsx
@@ -1,75 +1,65 @@
+import React from 'react';
+
 export const DetalleSpotify = () => {
   return (
-    <section className="text-white p-5 container mx-auto py-10 lg:py-20 ">
-      <h3 className="text-2xl lg:text-4xl pb-10">Escuchalas en Spotify</h3>
-      <div className="grid grid-cols-2 sm:grid-cols-4  lg:grid-cols-4 gap-3 my-5">
+    <section className="text-white p-5 container mx-auto py-10 lg:py-20">
+      <h3 className="text-2xl lg:text-4xl pb-10">Escúchalas en Spotify</h3>
+      <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-4 gap-3 my-5">
         <div className="space-y-3">
-          <img
-            src="https://tuentrada.com/concierto/rauw-alejandro/1.png"
-            alt=""
-          />
+          <iframe
+            style={{ borderRadius: '12px' }}
+            src="https://open.spotify.com/embed/track/2OZsedyLpIuOVRVODdhaMZ?utm_source=generator"
+            width="100%"
+            height="152"
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
           <hr />
           <p>Saturno</p>
-          <p>
-            <a
-              href="https://open.spotify.com/track/2OZsedyLpIuOVRVODdhaMZ?si=qzQlPpTGQSChy593rNMIWw "
-              target="blank"
-              className="underline inline-block"
-            >
-              Escuchar
-            </a>
-          </p>
         </div>
         <div className="space-y-3">
-          <img
-            src="https://tuentrada.com/concierto/rauw-alejandro/2.png"
-            alt=""
-          />
+          <iframe
+            style={{ borderRadius: '12px' }}
+            src="https://open.spotify.com/embed/track/2TDyyEBasw10wu5Uh94bR1?utm_source=generator"
+            width="100%"
+            height="152"
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
           <hr />
           <p>Punto 40</p>
-          <p>
-            <a
-              href="https://open.spotify.com/track/2OZsedyLpIuOVRVODdhaMZ?si=qzQlPpTGQSChy593rNMIWw "
-              target="blank"
-              className="underline inline-block"
-            >
-              Escuchar
-            </a>
-          </p>
         </div>
         <div className="space-y-3">
-          <img
-            src="https://tuentrada.com/concierto/rauw-alejandro/3.png"
-            alt=""
-          />
+          <iframe
+            style={{ borderRadius: '12px' }}
+            src="https://open.spotify.com/embed/track/6fmpK6IerfBrhZo4a2MYkC?utm_source=generator"
+            width="100%"
+            height="152"
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
           <hr />
           <p>Más de una vez</p>
-          <p>
-            <a
-              href="https://open.spotify.com/track/2OZsedyLpIuOVRVODdhaMZ?si=qzQlPpTGQSChy593rNMIWw "
-              target="blank"
-              className="underline inline-block"
-            >
-              Escuchar
-            </a>
-          </p>
         </div>
         <div className="space-y-3">
-          <img
-            src="https://tuentrada.com/concierto/rauw-alejandro/4.png"
-            alt=""
-          />
+          <iframe
+            style={{ borderRadius: '12px' }}
+            src="https://open.spotify.com/embed/track/39qtjJ454r2fgyXaenq8Wv?utm_source=generator"
+            width="100%"
+            height="152"
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
           <hr />
           <p>Lejos del cielo</p>
-          <p>
-            <a
-              href="https://open.spotify.com/track/2OZsedyLpIuOVRVODdhaMZ?si=qzQlPpTGQSChy593rNMIWw "
-              target="blank"
-              className="underline inline-block"
-            >
-              Escuchar
-            </a>
-          </p>
         </div>
       </div>
     </section>

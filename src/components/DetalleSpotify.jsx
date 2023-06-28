9443a3846a89b1450d752bfa99b3c1fa33464a5c
DaianaVanoniPuggioni <92957063+DaianaVanoniPuggioni@users.noreply.github.com>
2023-06-28 15:57:53 -0300

    cambio de iframe

--- src/components/DetalleSpotify.jsx
+++ src/components/DetalleSpotify.jsx
@@ -7,12 +7,12 @@ export const DetalleSpotify = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 my-5">
         <div className="space-y-3">
-          <SpotifyCard
+          {/* <SpotifyCard
             link={
               "https://open.spotify.com/track/2OZsedyLpIuOVRVODdhaMZ?si=1d4297bf3a97429f"
             }
-          />
-          {/* <iframe
+          /> */}
+          <iframe
             style={{ borderRadius: '12px' }}
             src="https://open.spotify.com/embed/track/2OZsedyLpIuOVRVODdhaMZ?utm_source=generator"
             width="100%"
@@ -21,37 +21,37 @@ export const DetalleSpotify = () => {
             allowFullScreen
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
             loading="lazy"
-          ></iframe> */}
+          ></iframe>
           <hr />
-          {/* <p className='text-base lg:text-lg'>Saturno</p> */}
+          <p className='text-base lg:text-lg'>Saturno</p>
         </div>
         <div className="space-y-3">
-          <SpotifyCard
+          {/* <SpotifyCard
             link={
               "https://open.spotify.com/track/2TDyyEBasw10wu5Uh94bR1?si=d1326544cd824fff"
             }
-          />
-          {/* <iframe
+          /> */}
+          <iframe
             style={{ borderRadius: '12px' }}
             src="https://open.spotify.com/embed/track/2TDyyEBasw10wu5Uh94bR1?utm_source=generator"
             width="100%"
             height="152"
             frameBorder="0"
-          
+
             allowFullScreen
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
             loading="lazy"
-          ></iframe> */}
+          ></iframe>
           <hr />
-          {/* <p className='text-base lg:text-lg'>Punto 40</p> */}
+          <p className='text-base lg:text-lg'>Punto 40</p>
         </div>
         <div className="space-y-3">
-          <SpotifyCard
+          {/* <SpotifyCard
             link={
               "https://open.spotify.com/track/6fmpK6IerfBrhZo4a2MYkC?si=2ec87bc18ec74f8c"
             }
-          />
-          {/* <iframe
+          /> */}
+          <iframe
             style={{ borderRadius: '12px' }}
             src="https://open.spotify.com/embed/track/6fmpK6IerfBrhZo4a2MYkC?utm_source=generator"
             width="100%"
@@ -60,17 +60,17 @@ export const DetalleSpotify = () => {
             allowFullScreen
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
             loading="lazy"
-          ></iframe> */}
+          ></iframe>
           <hr />
-          {/* <p className='text-base lg:text-lg'>Más de una vez</p> */}
+          <p className='text-base lg:text-lg'>Más de una vez</p>
         </div>
         <div className="space-y-3">
-          <SpotifyCard
+          {/* <SpotifyCard
             link={
               "https://open.spotify.com/track/39qtjJ454r2fgyXaenq8Wv?si=8e7b62be756341df"
             }
-          />
-          {/* <iframe
+          /> */}
+          <iframe
             style={{ borderRadius: '12px' }}
             src="https://open.spotify.com/embed/track/39qtjJ454r2fgyXaenq8Wv?utm_source=generator"
             width="100%"
@@ -79,9 +79,9 @@ export const DetalleSpotify = () => {
             allowFullScreen
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
             loading="lazy"
-          ></iframe> */}
+          ></iframe>
           <hr />
-          {/* <p className='text-base lg:text-lg'>Lejos del cielo</p> */}
+          <p className='text-base lg:text-lg'>Lejos del cielo</p>
         </div>
       </div>
     </section>

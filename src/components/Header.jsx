bcd023f4a8aa73adcbfaee445d24d80476591999
guido fortunato <guido_fortunato@hotmail.com.ar>
2023-06-30 16:23:10 -0300

    cambio validacion de error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,4 @@
-
-
 export const Header = () => {
-
-
-
-
   // const [showButton, setShowButton] = useState(false);
   // const targetDate = new Date('2023-06-27');
 
@@ -20,31 +14,36 @@ export const Header = () => {
   //   return () => clearInterval(interval);
   // }, []);
 
-
   return (
     <>
       <header
         className={`nav bg-no-repeat bg-cover bg-end z-40`}
-      
         // style={{
         //   backgroundImage: `url("https://www.tuentrada.com/concierto/rauw-alejandro/banner.png"})`,
         // }}
-      style={{padding:" 0 20px"}} >
-        <img className="sm:block md:hidden" src="https://tuentrada.com/concierto/rauw-alejandro/900x800.jpg" alt="banner principal de Rauw Alejandro" />
-        <img className="hidden md:block w-screen lg:hidden"  src="https://tuentrada.com/concierto/rauw-alejandro/800x400.jpg" alt="banner principal de Rauw Alejandro" />
-        <img className="hidden  lg:block w-screen"  src="https://tuentrada.com/concierto/rauw-alejandro/1920x710.webp" alt="banner principal de Rauw Alejandro" />
-      
-          
+        style={{ padding: "10px 20px" }}
+      >
+        <img
+          className="sm:block md:hidden"
+          src="https://tuentrada.com/concierto/rauw-alejandro/900x800.jpg"
+          alt="banner principal de Rauw Alejandro"
+        />
+        <img
+          className="hidden md:block w-screen lg:hidden"
+          src="https://tuentrada.com/concierto/rauw-alejandro/800x400.jpg"
+          alt="banner principal de Rauw Alejandro"
+        />
+        <img
+          className="hidden  lg:block w-screen"
+          src="https://tuentrada.com/concierto/rauw-alejandro/1920x710.webp"
+          alt="banner principal de Rauw Alejandro"
+        />
+
         {/* <div
           className={`min-h-[50vh] flex justify-center items-start flex-col `}
           >
           
         </div> */}
-      
-
-       
-    
-      
       </header>
     </>
   );
--- src/components/Preventas.jsx
+++ src/components/Preventas.jsx
@@ -3,11 +3,10 @@ import { CardsPreventa } from "./CardsPreventa";
 import { CardsHorario } from "./CardsHorario";
 import { BotonComprar } from "./";
 
-const dateToCompare = new Date("Thu Jun 30 2023 17:00:00 GMT-0300");
+const dateToCompare = new Date("Thu Jun 30 2023 19:00:00 GMT-0300");
 
 export const Preventas = () => {
   const [button, setButton] = useState(false);
-  const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [time, setTime] = useState(false);
   const [days, setDays] = useState("00");
@@ -23,17 +22,17 @@ export const Preventas = () => {
       setIsLoading(true);
       try {
         const response = await fetch("https://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires");
-        console.log({response})
+        // console.log({response})
         if (!response.ok) {
-          const data = await response.json();
-          setError(data.error);
+          setTime(new Date());
           return;
         }
         const data = await response.json();
         const currentDateTime = new Date(data.datetime);
+        console.log(currentDateTime)
         setTime(currentDateTime);
       } catch (error) {
-        setError(error);
+        // setError(error);
         throw new Error(error);
       } finally {
         setIsLoading(false);
@@ -83,31 +82,31 @@ export const Preventas = () => {
 
   if (isLoading) return <span></span>;
 
-  if (error !== null)
-    return (
-      <div
-        className="flex justify-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
-        role="alert"
-      >
-        <svg
-          aria-hidden="true"
-          className="flex-shrink-0 inline w-5 h-5 mr-3"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-        <span className="sr-only">Info</span>
-        <div>
-          Por favor intente nuevamente mas tarde
-        </div>
-      </div>
-    );
+  // if (error !== null)
+  //   return (
+  //     <div
+  //       className="flex t-5 justify-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+  //       role="alert"
+  //     >
+  //       <svg
+  //         aria-hidden="true"
+  //         className="flex-shrink-0 inline w-5 h-5 mr-3"
+  //         fill="currentColor"
+  //         viewBox="0 0 20 20"
+  //         xmlns="http://www.w3.org/2000/svg"
+  //       >
+  //         <path
+  //           fillRule="evenodd"
+  //           d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+  //           clipRule="evenodd"
+  //         ></path>
+  //       </svg>
+  //       <span className="sr-only">Info</span>
+  //       <div>
+  //         Por favor intente nuevamente mas tarde
+  //       </div>
+  //     </div>
+  //   );
 
   return (
     <section className="text-white container mx-auto pt-10">
